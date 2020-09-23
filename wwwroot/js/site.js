166fd85331a3ef3a21e91ca5a69631deafbfb640
Kate Fedak <katefedakk@gmail.com>
2020-09-23 18:12:41 +0300

    Using jQuery ajax function to help with the remote requests and responses to the Products API. LoadEmployee(), makes an HTTP GET over the respective API endpoint to retrieve the list of employee from the gRPC server project. Create delete operation for employee.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,43 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
+function LoadEmployee() {
+    $.ajax({
+        url: "/api/employee",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            var output = '';
+            $.each(result, function (key, item) {
+                output += '<tr>';
+                output += '<td>' + item.employeeId + '</td>';
+                output += '<td>' + item.firstname + '</td>';
+                output += '<td>' + item.lastname + '</td>';
+                output += '<td>' + item.salary + '</td>';
+                output += `<td><a href="#" class="btn redgate" onclick="SetUpEditModal(${item.employeeId})">Edit</a> |
+                        <a href="#" class="btn redgate" onclick="DeleteEmployee(${item.employeeId})">Delete</a></td>`;
+                output += '</tr>';
+            });
+            $('.tbody').html(output);
+        },
+        error: function (message) {
+            console.log(message.responseText);
+        }
+    });
+}
+function DeleteEmployee(id) {
+    if (confirm("Are you sure?")) {
+        $.ajax({
+            url: "/api/employee/" + id,
+            type: "DELETE",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function () {
+                LoadEmployee();
+            },
+            error: function (message) {
+                console.log(message.responseText);
+            }
+        });
+    }
+}
+LoadEmployee();
